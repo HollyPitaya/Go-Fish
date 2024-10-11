2622934330e7b02b3c547b97f0d9410c36aeb99b
HollyPitaya <kellycatgamer@gmail.com>
2024-10-11 08:39:29 -0600

    Update Game.py

--- Game.py
+++ Game.py
@@ -7,6 +7,7 @@ class Game():
         deckCount = deck.getLength()
         myBooks = 0
         aiBooks = 0
+        print("Welcome to Go Fish! Please note you and the computer will only gain books at the end of each turn.")
         while deckCount > 1:
             print("________________________________________________________________________________")
             print("Your hand is")
@@ -89,16 +90,21 @@ class Game():
     def myPlay(myHand, aiHand):
         choice = input("What will you ask for? Enter a number/Queen, jack, king, or ace\n")
         index = 0
+
         
 
         if choice == "Queen" or choice == "queen":
-            index = aiHand.hasCardAt(Card(12, ""))
+            choice = 12
+            index = aiHand.hasCardAt(Card(int(choice), ""))
         elif choice == "Jack" or choice == "jack":
-            index = aiHand.hasCardAt(Card(11, ""))
+            choice = 11
+            index = aiHand.hasCardAt(Card(int(choice), ""))
         elif choice == "King" or choice == "king":
-            index = aiHand.hasCardAt(Card(13, ""))
+            choice = 13
+            index = aiHand.hasCardAt(Card(int(choice), ""))
         elif choice == "Ace" or choice == "ace":
-            index = aiHand.hasCardAt(Card(1, ""))
+            choice = 1
+            index = aiHand.hasCardAt(Card(int(choice), ""))
         elif choice.isnumeric() == True:
             choice = int(choice)
             if choice > 0 and choice < 11:
@@ -111,10 +117,14 @@ class Game():
         if index >= 0:
             print("The computer had a " + str(choice) + " and gave you it")
             myHand.appendCard(aiHand.giveCard(index))
+            return Game.myPlay(myHand, aiHand)
         else:
             print("Go fish!")
             print("You drew a card")
             myHand.addCard()
+            if myHand.getCardAt(myHand.getLength() - 1).getNumber() == choice:
+                print("You got the card you wanted so you can play again")
+                return Game.myPlay(myHand, aiHand)
             
 
 
@@ -123,9 +133,11 @@ class Game():
         index = 0
         aiChoice = random.choice(aiChoices)
 
-        if aiHand.hasCardAt(Card(aiChoice, "")) >= 0:
-            return Game.aiPlay(myHand, aiHand)
-        
+        if aiHand.hasCardAt(Card(aiChoice, "")) < 0:
+            if aiHand.getLength() != 0:
+                return Game.aiPlay(myHand, aiHand)
+
+
         if aiChoice == 11:
             print("The computer asked for a Jack")
             index = myHand.hasCardAt(Card(aiChoice, ""))
@@ -142,8 +154,12 @@ class Game():
         if index >= 0:
             print("You gave the computer a card")
             aiHand.appendCard(myHand.giveCard(index))
+            return Game.aiPlay(myHand, aiHand)
         else:
             print("Go fish!")
             print("The computer drew a card")
             aiHand.addCard()
+            if aiHand.getCardAt(aiHand.getLength() - 1).getNumber() == aiChoice:
+                print("The computer got the card it wanted so it can play again")
+                return Game.aiPlay(myHand, aiHand)
 
